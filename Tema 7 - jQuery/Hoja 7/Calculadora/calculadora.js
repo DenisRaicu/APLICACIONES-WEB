8dba4639a9b6ab6a97beb60a0c79ef0a4f1a0aad
Denis <denisrai@ucm.es>
2019-02-14 01:06:59 +0100

    Add files via upload

---
+++ Tema 7 - jQuery/Hoja 7/Calculadora/calculadora.js
@@ -0,0 +1,11 @@
+
+
+$(() => {
+  console.log("DOM inicializado");
+  // Código de inicialización
+
+});
+
+// Define las funciones auxiliares y variables que creas necesarias
+
+// Puedes modificar el fichero HTML, si lo consideras necesario
